5afd8b453f3f651f96156ca142b97b5c989abbba
Игорь <crazydead256@gmail.com>
2021-04-30 23:30:18 +0300

    Наброски UI и модели данных

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -66,7 +66,7 @@ namespace HomeWork_8
                         }
                     }
                 }
-            }   //Тестовые  данные
+            }   //Тестовые данные
             );
             DeportamentView.ItemsSource = Deportaments;
         }
